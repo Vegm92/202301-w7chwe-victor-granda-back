8f20cfe510a6ea82b2cf190b055c3694284b37ab
Victor Granda <victorgranda1992@gmail.com>
2023-02-25 16:47:03 +0100

    Add logic for registerUser and hashed password

--- src/server/controllers/userControllers.ts
+++ src/server/controllers/userControllers.ts
@@ -1,16 +1,32 @@
 import { type NextFunction, type Request, type Response } from "express";
+import bcryptjs from "bcryptjs";
 import { CustomError } from "../../CustomError/CustomError.js";
 import { User } from "../../database/models/User.js";
 import { type UserRegister } from "./types.js";
 
-export const createUser = async (
+export const registerUser = async (
   req: Request<Record<string, unknown>, Record<string, unknown>, UserRegister>,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const { username, password, avatar, email } = req.body;
-  } catch (error) {}
+    const { username, password, email } = req.body;
+    const hashedPassword = await bcryptjs.hash(password, 8);
+    const avatar = req.file?.filename;
+
+    await User.create({ username, hashedPassword, avatar, email });
+    const message = "User registered successfully";
+
+    res.status(201).json({ username, message });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Error registering a user"
+    );
+
+    next(customError);
+  }
 };
 
 export const getUsers = async (
