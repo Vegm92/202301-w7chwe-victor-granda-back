05df7097e8f58c509a6a972d44a0239ef1434eae
Victor Granda <victorgranda1992@gmail.com>
2023-02-25 17:04:00 +0100

    Add storage and register method to router

--- src/server/routers/usersRouters.ts
+++ src/server/routers/usersRouters.ts
@@ -1,7 +1,25 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/userControllers.js";
+import { v4 as uuidv4 } from "uuid";
+import multer from "multer";
+import { getUsers, registerUser } from "../controllers/userControllers.js";
 
 export const usersRouters = Router();
 
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename(req, file, callback) {
+    const suffix = uuidv4();
+    const mimetype = file.mimetype.split("/");
+    const extension = mimetype[mimetype.length - 1];
+    const processedFilename = `${file.fieldname}-${suffix}.${extension}`;
+    callback(null, processedFilename);
+  },
+});
+
+const upload = multer({ storage });
+const uploadAvatar = upload.single("avatar");
+
 usersRouters.get("/", getUsers);
-usersRouters.post("/login/");
+usersRouters.post("/register", uploadAvatar, registerUser);
+
+export default usersRouters;
